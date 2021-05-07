9eb6e2ee24fd325f97936a829b9e198e1a9f2115
DoctorMario147 <77082199+DoctorMario147@users.noreply.github.com>
2021-05-07 14:24:24 +0100

    Updated style of file
    Made file follow PEP-8 guidelines, and added much needed comments.

--- main.py
+++ main.py
@@ -2,40 +2,53 @@ import requests
 from bs4 import BeautifulSoup
 import discord
 
+
 client = discord.Client()
 
+
+# When the bot declares itself as 'ready' for use
 @client.event
 async def on_ready():
+    # Print to the console that the bot is logged on
     print("We have logged in as {0.user}".format(client))
 
+
+# When a message is sent
 @client.event
 async def on_message(message):
+    # Don't run any commands if the command is the bot itself
     if message.author == client.user:
         return
 
-    if message.content.lower() == ("!bbc"):
+    # If the message (when converted to lower case) is !bbc, run the command
+    if message.content.lower() == "!bbc":
+        # Request BBC homepage and news for later use
         bbchome = requests.get("https://www.bbc.co.uk")
         bbchomesoup = BeautifulSoup(bbchome.text, "html.parser")
         bbcnews = requests.get("https://www.bbc.co.uk/news")
         bbcnewssoup = BeautifulSoup(bbcnews.text, "html.parser")
 
+        # Scrape homepage to find the top story, and then assign variables
         topheadline = bbchomesoup.find(class_="ssrcss-egasky-Promo ett16tt0").find("p").find("span")
         topheadline = topheadline.text
         toplink = bbchomesoup.find(class_="ssrcss-egasky-Promo ett16tt0").find("a")
         toplink = toplink.get("href")
 
+        # Scrape BBC News to find the most-read story, and then assign variables
         mostreadheadline = bbcnewssoup.find(
-            class_="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs").find(
-            "span")
+            class_="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold"
+                   " gs-u-pl-@xs").find("span")
         mostreadheadline = mostreadheadline.text
         mostreadlink = bbcnewssoup.find(
-            class_="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs")
+            class_="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold"
+                   " gs-u-pl-@xs")
         mostreadlink = "https://www.bbc.co.uk" + mostreadlink.get("href")
 
-
+        # Send messages to channel where command was called
         await message.channel.send(f"**TOP STORY:** {topheadline}\n"
                                    f"{toplink}")
         await message.channel.send(f"**MOST READ STORY:** {mostreadheadline}\n"
                                    f"{mostreadlink}")
 
+# Actually runs the bot with the unique token
 client.run("TOKEN")
